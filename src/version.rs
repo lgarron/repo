2c71e8ba44c90b6b9d12d86c583ba42151e8bf97
Lucas Garron <code@garron.net>
2024-11-14 23:30:19 -0800

    Support `npm` for `repo version` commands.

--- src/version.rs
+++ src/version.rs
@@ -1,18 +1,22 @@
 use std::fmt::Display;
+use std::fs::File;
 use std::process::Command;
 
-use clap::{Args, Subcommand};
+use clap::{Args, Subcommand, ValueEnum};
 
 use cargo_metadata::MetadataCommand;
+use serde::Deserialize;
 
 #[derive(Args, Debug)]
 pub(crate) struct VersionArgs {
     #[command(subcommand)]
-    pub command: VersionCommand,
+    command: VersionCommand,
+    #[command(flatten)]
+    ecosystem_args: EcosystemArgs,
 }
 
 #[derive(Debug, Subcommand)]
-pub(crate) enum VersionCommand {
+enum VersionCommand {
     /// Get the current version
     Get(VersionGetArgs),
     /// Bump the current version
@@ -20,20 +24,20 @@ pub(crate) enum VersionCommand {
 }
 
 #[derive(Args, Debug)]
-pub(crate) struct VersionGetArgs {
+struct VersionGetArgs {
     /// Do not print the `v` prefix (e.g. print `0.1.3` instead of `v0.1.3`)
     #[clap(long)]
     pub no_prefix: bool,
 }
 
 #[derive(Args, Debug)]
-pub(crate) struct VersionBumpArgs {
+struct VersionBumpArgs {
     #[command(subcommand)]
     pub command: VersionBumpCommand,
 }
 
 #[derive(Debug, Subcommand)]
-pub(crate) enum VersionBumpCommand {
+enum VersionBumpCommand {
     Major,
     Minor,
     Patch,
@@ -53,29 +57,100 @@ impl Display for VersionBumpCommand {
     }
 }
 
+#[derive(Args, Debug)]
+struct EcosystemArgs {
+    #[clap(long, default_value = "auto")]
+    pub ecosystem: RepoEcosystem,
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+enum RepoEcosystem {
+    Auto,
+    Npm,
+    Cargo,
+}
+
+#[derive(Deserialize)]
+struct PackageJSONWithVersion {
+    version: String,
+}
+
+const PACKAGE_JSON_PATH: &str = "./package.json";
+
+fn npm_get_version() -> Result<String, String> {
+    // TODO: semantically parse version
+    let Ok(file) = File::open(PACKAGE_JSON_PATH) else {
+        return Err("Could not read `package.json`".to_owned());
+    };
+    let Ok(package_json) = serde_json::from_reader::<_, PackageJSONWithVersion>(file) else {
+        return Err("Could not read `package.json`".to_owned());
+    };
+    Ok(package_json.version)
+}
+
+fn cargo_get_version() -> String {
+    MetadataCommand::new()
+        .manifest_path("./Cargo.toml")
+        .current_dir(".")
+        .exec()
+        .unwrap()
+        .root_package()
+        .unwrap()
+        .version
+        .to_string()
+}
+
+fn print_version(version: &str, version_get_args: &VersionGetArgs) {
+    let prefix = if version_get_args.no_prefix { "" } else { "v" };
+    println!("{}{}", prefix, version);
+}
+
+fn npm_bump_version(version_bump_command: VersionBumpCommand) {
+    Command::new("npm")
+        .args([
+            "version",
+            "--no-git-tag-version",
+            &version_bump_command.to_string(),
+        ])
+        .status()
+        .expect("Could not bump version using `npm`");
+}
+
+fn cargo_bump_version(version_bump_command: VersionBumpCommand) {
+    println!("Assuming `cargo-bump` is installed…");
+    Command::new("cargo")
+        .args(["bump", &version_bump_command.to_string()])
+        .status()
+        .expect("Could not bump version using `cargo-bump`");
+}
+
+// TODO: use traits to abstract across ecosystems
+// TODO: support cross-checking versions across ecosystems
 pub(crate) fn version_command(version_args: VersionArgs) {
     match version_args.command {
         VersionCommand::Get(version_get_args) => {
-            let prefix = if version_get_args.no_prefix { "" } else { "v" };
-            println!(
-                "{}{}",
-                prefix,
-                MetadataCommand::new()
-                    .manifest_path("./Cargo.toml")
-                    .current_dir(".")
-                    .exec()
-                    .unwrap()
-                    .root_package()
-                    .unwrap()
-                    .version
-            );
+            let version = match version_args.ecosystem_args.ecosystem {
+                RepoEcosystem::Auto => match npm_get_version() {
+                    Ok(version) => version,
+                    Err(_) => cargo_get_version(),
+                },
+                RepoEcosystem::Npm => {
+                    npm_get_version().expect("Could not get `npm` package version.")
+                }
+                RepoEcosystem::Cargo => cargo_get_version(),
+            };
+            print_version(&version, &version_get_args);
         }
-        VersionCommand::Bump(version_bump_args) => {
-            println!("Assuming `cargo-bump` is installed…");
-            Command::new("cargo")
-                .args(["bump", &version_bump_args.command.to_string()])
-                .status()
-                .expect("Could not bump version");
-        }
+        VersionCommand::Bump(version_bump_args) => match version_args.ecosystem_args.ecosystem {
+            RepoEcosystem::Auto => {
+                if npm_get_version().is_ok() {
+                    npm_bump_version(version_bump_args.command)
+                } else {
+                    cargo_bump_version(version_bump_args.command)
+                }
+            }
+            RepoEcosystem::Npm => npm_bump_version(version_bump_args.command),
+            RepoEcosystem::Cargo => cargo_bump_version(version_bump_args.command),
+        },
     }
 }

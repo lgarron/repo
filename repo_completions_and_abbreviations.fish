ab0017a4d59b45f3314ab010a2c79b4bd0b3234c
Lucas Garron <code@garron.net>
2024-11-15 02:52:31 -0800

    [repo_completions_and_abbreviations.fish] Refine completions.
    This also avoids putting any completions in the command namespace other than `p`.

--- repo_completions_and_abbreviations.fish
+++ repo_completions_and_abbreviations.fish
@@ -9,22 +9,23 @@ repo completions fish | source
 
 abbr -a "p" "repo"
 
-abbr -a "v" "repo version"
-abbr -a "version" "repo version"
 abbr_subcommand repo v "version"
-
-abbr -a "vb" "repo version bump"
-abbr -a "vbm" "repo version bump minor"
-abbr -a "vbp" "repo version bump patch"
-abbr_subcommand repo bump "version bump"
+abbr_subcommand_arg repo b bump version
+abbr_subcommand repo vm "version bump minor"
+abbr_subcommand repo vp "version bump patch"
 abbr_subcommand repo major "version bump major"
 abbr_subcommand repo minor "version bump minor"
 abbr_subcommand repo patch "version bump patch"
 
 abbr_subcommand repo p "publish"
-abbr -a "pp" "repo publish"
 
 abbr_subcommand repo b "boilerplate"
+abbr_subcommand repo ci "boilerplate ci"
+abbr_subcommand repo gr "boilerplate auto-publish-github-release"
+abbr_subcommand_arg repo c create boilerplate
+abbr_subcommand_arg repo e edit boilerplate
+abbr_subcommand_arg repo r reveal boilerplate
 
 abbr_subcommand repo s "setup"
-abbr_subcommand repo deps "setup dependencies"
+abbr_subcommand repo d "setup dependencies"
+abbr_subcommand_arg repo d dependencies setup

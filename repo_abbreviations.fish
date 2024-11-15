a6f7b9ff047501c854978989aa24aed22017a08e
Lucas Garron <code@garron.net>
2024-11-14 22:45:55 -0800

    Add abbreviations for `fish`.

---
+++ repo_abbreviations.fish
@@ -0,0 +1,7 @@
+# Usage: source "repo_abbreviations.fish"
+
+repo completions fish | source
+abbr_subcommand repo bump "version bump"
+abbr_subcommand repo major "version bump major"
+abbr_subcommand repo minor "version bump minor"
+abbr_subcommand repo patch "version bump patch"

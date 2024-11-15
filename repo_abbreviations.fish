62b57ee9860ef74f36e3d6317eb18bd3cf34aabc
Lucas Garron <code@garron.net>
2024-11-14 22:59:16 -0800

    [repo_abbreviations.fish] Add a warning if `abbr_subcommand` is not defined.

--- repo_abbreviations.fish
+++ repo_abbreviations.fish
@@ -1,5 +1,10 @@
 # Usage: source "repo_abbreviations.fish"
 
+if not functions abbr_subcommand > /dev/null
+  echo "These abbreviations require: https://github.com/lgarron/dotfiles/blob/e5227c8aec1fdf8ffdd3fa3e0a57b934e5f8f9bd/dotfiles/fish/.config/fish/abbr.fish" >&2
+  exit 1
+end
+
 repo completions fish | source
 
 abbr_subcommand repo v "version"

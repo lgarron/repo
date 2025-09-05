4664af1f4174973c9e379a498b4663c81ffb1c7a
Lucas Garron <code@garron.net>
2025-09-05 01:47:25 -0700

    test

--- src/js/@lgarron-repo/repo/repo.js
+++ src/js/@lgarron-repo/repo/repo.js
@@ -1,8 +1,7 @@
-#!/usr/bin/env node --
-
 import { spawn } from "node:child_process";
 import { existsSync } from "node:fs";
 import { argv, exit } from "node:process";
+import { fileURLToPath } from "node:url";
 
 for (const architectureTriple of [
   "aarch64-apple-darwin",
@@ -10,7 +9,16 @@ for (const architectureTriple of [
   "x86_64-apple-darwin",
   "x86_64-pc-windows",
 ]) {
-  const path = import.meta.resolve(`@lgarron-repo/repo-${architectureTriple}`);
+  let path;
+  try {
+    path = fileURLToPath(
+      import.meta.resolve(`@lgarron-repo/repo-${architectureTriple}`),
+    );
+  } catch (e) {
+    if (e.code === "ERR_MODULE_NOT_FOUND") {
+      continue;
+    }
+  }
   if (await existsSync(path)) {
     let command;
     try {

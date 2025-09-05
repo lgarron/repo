2a9c1b74d367764f3a3c544f78b93252ae6b4da6
Lucas Garron <code@garron.net>
2025-09-05 03:07:48 -0700

    Add support for an `REPO_DEBUG_NPM_RESOLUTION=true` env var.

--- src/js/@lgarron-repo/repo/repo.js
+++ src/js/@lgarron-repo/repo/repo.js
@@ -2,8 +2,11 @@
 
 import { spawn } from "node:child_process";
 import { existsSync } from "node:fs";
-import { argv, exit } from "node:process";
+import { argv, env, exit } from "node:process";
 import { fileURLToPath } from "node:url";
+
+// biome-ignore lint/complexity/useLiteralKeys: https://github.com/biomejs/biome/issues/463 or https://github.com/biomejs/biome/issues/6736
+const DEBUG = env["REPO_DEBUG_NPM_RESOLUTION"] === "true";
 
 for (const architectureTriple of [
   "aarch64-apple-darwin",
@@ -11,24 +14,58 @@ for (const architectureTriple of [
   "x86_64-apple-darwin",
   "x86_64-pc-windows",
 ]) {
+  if (DEBUG) {
+    console.error(
+      `--------
+[${architectureTriple}] Testing architecture triple: ${architectureTriple}`,
+    );
+  }
   let path;
   try {
     path = fileURLToPath(
       import.meta.resolve(`@lgarron-repo/repo-${architectureTriple}`),
     );
+    if (DEBUG) {
+      console.error(`[${architectureTriple}] Resolved to path: `, e);
+    }
   } catch (e) {
     if (e.code === "ERR_MODULE_NOT_FOUND") {
+      if (DEBUG) {
+        console.error(
+          `[${architectureTriple}] Failed to resolve. Continuing to next architecture.`,
+        );
+      }
       continue;
+    }
+    if (DEBUG) {
+      console.error(
+        `[${architectureTriple}] Unexpected error during resolution: `,
+        e,
+      );
     }
     throw e;
   }
   if (await existsSync(path)) {
+    if (DEBUG) {
+      console.error(`[${architectureTriple}] Path exists: `, path);
+    }
     let command;
     try {
       command = spawn(path, argv.slice(2), { stdio: "inherit" });
     } catch (e) {
       if (e.code === "EBADARCH") {
+        if (DEBUG) {
+          console.error(
+            `[${architectureTriple}] Bad architecture. Continuing to next architecture`,
+          );
+        }
         continue;
+      }
+      if (DEBUG) {
+        console.error(
+          `[${architectureTriple}] Unexpected error during binary launch: `,
+          e,
+        );
       }
       throw e;
     }

55092e2c9379396cd9ee167dcff39f2c49a6f208
Lucas Garron <code@garron.net>
2025-09-09 23:47:09 -0700

    Add documentation to `--fallback`.

--- src/commands/workspace.rs
+++ src/commands/workspace.rs
@@ -30,6 +30,10 @@ pub(crate) struct WorkspaceRootArgs {
 
 #[derive(Debug, Clone, ValueEnum, PartialEq, Eq)]
 enum RootFallback {
+    /// Use either the path itself (if it's an existing directory) or its parent (if it's not).
+    ///
+    /// Note: due to Rust parsing quirks, non-existent paths are always treated as non-directories (even if they have a trailing slash), i.e. their parent will be returned.
+    // TODO: always treat paths with a trailing slash as dirs.
     #[clap(name = "closest-dir")]
     ClosestDir,
 }
@@ -38,23 +42,28 @@ pub(crate) fn workspace_command(workspace_args: WorkspaceArgs) {
     match workspace_args.command {
         WorkspaceCommand::Root(workspace_root_args) => {
             let path = &workspace_root_args.path_args.path();
-            if let Some(path) = auto_detect_workspace_root(path) {
-                print!("{}", path)
+            let root_path = if let Some(path) = auto_detect_workspace_root(path) {
+                path
             } else {
                 match workspace_root_args.fallback {
                     Some(RootFallback::ClosestDir) => {
+                        // TODO: wire things up so that we can tell if the argument had a trailing slash. Probably requires asking `clap` to keep parse into a `String` an `PathBuf` at the same time.
                         if path.is_dir() {
-                            print!("{}", path.to_string_lossy());
-                            return;
+                            path.to_string_lossy().to_string()
                         } else if let Some(parent_path) = path.parent() {
-                            print!("{}", parent_path.to_string_lossy());
-                            return;
+                            parent_path.to_string_lossy().to_string()
+                        } else {
+                            eprintln!("Could not get parent path");
+                            exit(1);
                         }
                     }
-                    None => {}
+                    None => {
+                        eprintln!("No workspace found. Consider passing: `--fallback closest-dir`");
+                        exit(1)
+                    }
                 }
-                exit(1)
-            }
+            };
+            print!("{}", root_path)
         }
     };
 }
--- src/common/args.rs
+++ src/common/args.rs
@@ -5,6 +5,7 @@ use clap::Args;
 #[derive(Args, Debug)]
 pub(crate) struct PathArgs {
     /// Path to an existing file or folder to use. Defaults to the current working directory.
+    // TODO: track if a trailing slash was specified (for `repo workspace root --fallback closest-dir …`), without affecting parsing or completion semantics
     #[clap(long = "path")]
     maybe_path: Option<PathBuf>,
 }

5c648bc7d16d84019ce79cb636ca2c1bf74c106f
Lucas Garron <code@garron.net>
2025-09-09 23:26:07 -0700

    Add `--fallback closest-dir` to `repo workspace root`.

--- src/commands/workspace.rs
+++ src/commands/workspace.rs
@@ -1,6 +1,6 @@
 use std::process::exit;
 
-use clap::{Args, Subcommand};
+use clap::{Args, Subcommand, ValueEnum};
 
 use crate::common::{args::PathArgs, workspace::auto_detect_workspace_root};
 
@@ -16,15 +16,41 @@ enum WorkspaceCommand {
     /// If the folder is part of multiple repositories, at most one will be returned (consistent with the `kind` subcommand).
     ///
     /// Also consider `repo vcs root` if you are only looking for VCS roots.
-    Root(PathArgs),
+    Root(WorkspaceRootArgs),
+}
+
+#[derive(Args, Debug)]
+pub(crate) struct WorkspaceRootArgs {
+    #[clap(long)]
+    fallback: Option<RootFallback>,
+
+    #[command(flatten)]
+    path_args: PathArgs,
+}
+
+#[derive(Debug, Clone, ValueEnum, PartialEq, Eq)]
+enum RootFallback {
+    #[clap(name = "closest-dir")]
+    ClosestDir,
 }
 
 pub(crate) fn workspace_command(workspace_args: WorkspaceArgs) {
     match workspace_args.command {
         WorkspaceCommand::Root(workspace_root_args) => {
-            if let Some(path) = auto_detect_workspace_root(&workspace_root_args.path()) {
+            let path = &workspace_root_args.path_args.path();
+            if let Some(path) = auto_detect_workspace_root(path) {
                 print!("{}", path)
             } else {
+                match workspace_root_args.fallback {
+                    Some(RootFallback::ClosestDir) => {
+                        if path.is_dir() {
+                            print!("{}", path.to_string_lossy())
+                        } else if let Some(parent_path) = path.parent() {
+                            print!("{}", parent_path.to_string_lossy())
+                        }
+                    }
+                    None => {}
+                }
                 exit(1)
             }
         }

7d3c185fe370d709ce67d7e98de926f4f62e2312
Lucas Garron <code@garron.net>
2025-10-13 15:34:59 -0700

    Add `aarch64-pc-windows` target architecture.
    Hopefully closes https://github.com/lgarron/repo/issues/8

--- src/js/@lgarron-bin/repo/repo.js
+++ src/js/@lgarron-bin/repo/repo.js
@@ -12,8 +12,9 @@ for (const architectureTriple of [
   "aarch64-apple-darwin",
   "x86_64-unknown-linux-gnu",
   "x86_64-apple-darwin",
-  "x86_64-pc-windows",
   "aarch64-unknown-linux-gnu",
+  "x86_64-pc-windows",
+  "aarch64-pc-windows",
 ]) {
   if (DEBUG) {
     console.error(

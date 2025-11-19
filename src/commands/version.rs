0940e25b88fec55af40f1985f034e45bd8480250
Lucas Garron <code@garron.net>
2025-11-18 17:52:34 -0800

    Ensure we can get a version description via `jj` even when `latest(tags())` is not an ancestor of `@-`.

--- src/commands/version.rs
+++ src/commands/version.rs
@@ -294,7 +294,7 @@ fn version_describe_and_print(version_describe_args: &VersionDescribeArgs) {
             // Based on https://github.com/jj-vcs/jj/discussions/2563#discussioncomment-11885001
             let mut jj_command = PrintableShellCommand::new("jj");
             jj_command.args(["log", "--no-graph", "--reversed"]);
-            jj_command.args(["--revisions", "latest(tags())::@-"]);
+            jj_command.args(["--revisions", "latest(tags()::@-)"]);
             jj_command.args([
                 "--template",
                 "commit_id.short(8) ++ \" \" ++ tags ++ \"\n\"",
